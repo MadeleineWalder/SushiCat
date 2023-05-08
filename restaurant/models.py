6b22b2bcab332b35d3048778ae63c91639d3df58
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-05-08 09:34:32 +0000

    Add superuser. Build booking model to create, read, edit and delete bookings

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
+# date
+# time 
+# no. of people
+# edit 
+# delete
+
 
 # Create your models here.
+class Booking(models.Model):
+    booking_name = models.CharField(max_length=20)
+    date = models.DateField()
+    time = models.TimeField()
+    number_of_people = models.IntegerField()
+
+
+# class Edit(models.Model):
+
+
+# class Delete(models.Model):
