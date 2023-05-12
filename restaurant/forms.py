0e186322d54332a7412643ab28241f8025d5db2f
MadeleineWalder <madeleinezoewalder@gmail.com>
2023-05-12 09:37:47 +0000

    Add forms.py

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.db import models
 from .models import Booking
 
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['booking_name', 'date', 'time', 'number_of_people']
+        fields = [
+            'booking_name',
+            'date',
+            'time',
+            'number_of_people'
+        ]
